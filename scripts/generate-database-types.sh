330d8c26d14920d2741a902f0935d405bd59847b
torresio <torresus@gmail.com>
2026-08-02 15:17:40 -0500

    Commit generated local database types

--- scripts/generate-database-types.sh
+++ scripts/generate-database-types.sh
@@ -6,5 +6,6 @@ types_output="$(mktemp "${TMPDIR:-/tmp}/opensourcingos-database-types.XXXXXX")"
 trap 'rm -f "${types_output}"' EXIT
 
 supabase gen types typescript --local --schema public > "${types_output}"
+perl -0pi -e 's/\n+\z/\n/' "${types_output}"
 mv "${types_output}" lib/database.types.ts
 trap - EXIT
